f8dd8371443352eb71637202fe0cfe62a944320a
Misael Aparecido Reis Cândido <misaelcandido@ENG-317.local>
2019-09-28 13:29:11 -0300

    update

--- features/support/web/pages/order.rb
+++ features/support/web/pages/order.rb
@@ -2,7 +2,7 @@ class OrderPage
     include Capybara::DSL
 
     def coast 
-        div = find("div[class*=col-sm-6)]", text: "Frete e Total:")
+        div = find("div[class*=col-sm-6]", text: "Frete e Total:")
         div.all("table tbody tr")
     end
 end
